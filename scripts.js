bebd83e6d965fa9bc80d0d73d356375f96bcf195
mooniiDev <moonii@riseup.net>
2021-03-10 17:14:26 +0200

    Add module for new game window and function to generate gameboard, update structure and background image, improve alignment

--- scripts.js
+++ scripts.js
@@ -15,41 +15,7 @@ const newGame = (() => {
       });
     });
   };
-  const selectGalaxy = () => {
-    const galaxies = document.querySelectorAll('.galaxy');
-    const gameBody = document.querySelector('#body');
-    galaxies.forEach((galaxy) => {
-      galaxy.addEventListener('click', () => {
-        gameBody.classList.remove('body-bg', 'andromeda', 'black-eye', 'fireworks', 'milky-way');
-        if (galaxy.id === 'andromeda') {
-          gameBody.classList.add('andromeda');
-        } else if (galaxy.id === 'black-eye') {
-          gameBody.classList.add('black-eye');
-        } else if (galaxy.id === 'fireworks') {
-          gameBody.classList.add('fireworks');
-        } else if (galaxy.id === 'milky-way') {
-          gameBody.classList.add('milky-way');
-        }
-      });
-    });
-  };
-  const startGame = () => {
-    const playGame = document.querySelector('.play');
-    const gameSelectionWindow = document.querySelector('#game-selection-window');
-    const gameWindow = document.querySelector('#game-window');
-    playGame.addEventListener('click', () => {
-      gameSelectionWindow.className = 'display-none';
-      gameWindow.className = 'display-block';
-    });
-  };
-  selectGameType();
-  selectGalaxy();
-  startGame();
-  return {};
-})();
-
-const iconsSelection = (() => {
-  const selectAvatars = () => {
+  const selectAvatar = () => {
     const avatars = document.querySelectorAll('.character i');
     avatars.forEach((avatar) => {
       avatar.addEventListener('click', () => {
@@ -69,7 +35,7 @@ const iconsSelection = (() => {
       });
     });
   };
-  const selectMarks = () => {
+  const selectMark = () => {
     const marks = document.querySelectorAll('.mark i');
     marks.forEach((mark) => {
       mark.addEventListener('click', () => {
@@ -89,28 +55,67 @@ const iconsSelection = (() => {
       });
     });
   };
+  const selectGalaxy = () => {
+    const galaxies = document.querySelectorAll('.galaxy');
+    const gameBody = document.querySelector('#body');
+    galaxies.forEach((galaxy) => {
+      galaxy.addEventListener('click', () => {
+        gameBody.classList.remove('body-bg', 'andromeda', 'black-eye', 'fireworks', 'milky-way');
+        if (galaxy.id === 'andromeda') {
+          gameBody.classList.add('andromeda');
+        } else if (galaxy.id === 'black-eye') {
+          gameBody.classList.add('black-eye');
+        } else if (galaxy.id === 'fireworks') {
+          gameBody.classList.add('fireworks');
+        } else if (galaxy.id === 'milky-way') {
+          gameBody.classList.add('milky-way');
+        }
+      });
+    });
+  };
   const computerSelection = () => {
     const aiAvatars = document.querySelectorAll('#enemy-ai .character i');
     const selectAiAvatar = aiAvatars[Math.floor(Math.random() * aiAvatars.length)];
     if (selectAiAvatar.classList.contains('fa-pastafarianism')) {
       selectAiAvatar.classList.add('theme-pastafarianism');
-      aiAvatars[1].classList.remove('theme-alien');
     } else if (selectAiAvatar.classList.contains('fa-alien-monster')) {
       selectAiAvatar.classList.add('theme-alien');
-      aiAvatars[0].classList.remove('theme-pastafarianism');
     }
     const aiMarks = document.querySelectorAll('#enemy-ai .mark i');
     const selectAiMark = aiMarks[Math.floor(Math.random() * aiMarks.length)];
     if (selectAiMark.classList.contains('fa-bacterium')) {
       selectAiMark.classList.add('theme-bacterium');
-      aiMarks[1].classList.remove('theme-virus');
     } else if (selectAiMark.classList.contains('fa-virus')) {
       selectAiMark.classList.add('theme-virus');
-      aiAvatars[0].classList.remove('theme-bacterium');
     }
   };
-  selectAvatars();
-  selectMarks();
+  const startPlay = () => {
+    const playBtn = document.querySelector('.play-btn');
+    const gameSelectionWindow = document.querySelector('#game-selection-window');
+    const gameWindow = document.querySelector('#game-window');
+    playBtn.addEventListener('click', () => {
+      gameSelectionWindow.className = 'display-none';
+      gameWindow.className = 'display-block';
+    });
+  };
+  selectGameType();
+  selectAvatar();
+  selectMark();
+  selectGalaxy();
   computerSelection();
+  startPlay();
   return {};
+})();
+
+const playGame = (() => {
+  const generateGrid = () => {
+    const gameboard = document.querySelector('#gameboard');
+    gameboard.innerHTML = '';
+    for (let i = 0; i < 9; i += 1) {
+      const gridDiv = document.createElement('div');
+      gameboard.classList.add('grid-3x3');
+      gameboard.appendChild(gridDiv);
+    }
+  };
+  generateGrid();
 })();
